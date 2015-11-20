41fdf0f8ab414e8732533d3def3d936678b05022
Giorgos Papoutsis <giorgos1987@users.noreply.github.com>
2015-11-20 15:39:58 +0200

    Create run_dc_toagris.sh

---
+++ run_dc_toagris.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#dir at testing agris vm /home/agris/JAVA/XSL_TRANSFORMER/translet-master
+
+#usage java -jar gr.agroknow.metadata.translet.Translet <xsl file> <input folder> <output folder> <bad folder>
+
+
+
+$TRANSLET=~/JAVA/XSL_TRANSFORMER/translet-master/dist/translet.jar
+#/home/agris/JAVA/XSL_TRANSFORMER/translet-master/dist/translet.jar
+$XSL=/home/agris/JAVA/XSL_TRANSFORMER/translet-master/XSL/DC_OAI_Final.xsl
+#AGRIS_to_OpenAIRE.xsl
+ #DC_OAI_Final.xsl
+$IN=/home/agris/JAVA/XSL_TRANSFORMER/translet-master/IN
+$OUT=/home/agris/JAVA/XSL_TRANSFORMER/translet-master/OUT
+$BAD=/home/agris/JAVA/XSL_TRANSFORMER/translet-master/BAD
+
+
+
+
+java -jar $TRANSLET $XSL $IN $OUT $BAD > txt
+
+
+echo "transforming done!"
+
+exit;

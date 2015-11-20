30a4ecc4b9e18798c2c8d733ee87eaaf240bae4c
Giorgos Papoutsis <giorgos1987@users.noreply.github.com>
2015-11-20 15:41:28 +0200

    Create setIngestAL1.sh

---
+++ setIngestAL1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SET=AL1
+FORMAT=AGRIS
+OAITARGET="http://agris.fao.org/oai"
+OAIFORMAT="agrisap"
+
+ROOT=/home/agris
+HARVESTER=${ROOT}/JAVA/harvester/harvestingSetD.jar
+JAVA=/usr/bin/java
+OUTPUTDIR=${ROOT}/agrisMetaCat/${SET}
+#/home/agris/agrisMetaCat/AL1
+
+#
+FROM=2015-07-15
+#2000-01-01
+UNTIL=2015-09-15
+#2015-07-15
+#2016-01-01
+
+
+#   ${JAVA} -jar ${HARVESTER} ${OAITARGET} ${OUTPUTDIR}/ ${OAIFORMAT}  ${UNTIL} ${FROM}  ${oaiset}
+
+
+
+
+for oaiset in AL1
+do
+  echo "harvesting AL1" ${set}
+#  ${JAVA} -jar ${HARVESTER} ${OAITARGET} ${OUTPUTDIR}/ ${OAIFORMAT} ${oaiset}
+${JAVA} -jar ${HARVESTER} ${OAITARGET} ${OUTPUTDIR}/ ${OAIFORMAT}  ${UNTIL} ${FROM}  ${oaiset}
+
+done
+echo "Harvesting AL1 Done!"

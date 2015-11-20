1ef201888f55f1cd4e08a4821e7d71bb9fc03380
Giorgos Papoutsis <giorgos1987@users.noreply.github.com>
2015-11-20 15:38:11 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+
+#Depricated
+
+./convertRDF.sh /home/agris/agris_app/workspace/work/work/AGRIS_ISSN_URI.txt /home/agris/agris_app/workspace/work/work/AGRIS_ARN_ISSN.txt /home/agris/agris_app/workspace/work/work/agrovocURILabelMappings.txt ~/agrisMetaCat/Sept14/MY0 /home/agris/agris_app/workspace/RDF_OUT_myo

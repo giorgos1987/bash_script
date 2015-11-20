9c113759f8b9f47d8eb319b0a28cc2f91007b629
Giorgos Papoutsis <giorgos1987@users.noreply.github.com>
2015-11-20 15:42:52 +0200

    Create ingestIFPRI.sh

---
+++ ingestIFPRI.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+SET=p15738coll2
+FORMAT=oai_dc
+
+OAITARGET="http://cdm15738.contentdm.oclc.org/oai/oai.php"
+
+OAIFORMAT="oai_dc"
+
+ROOT=/home/agris
+HARVESTER=${ROOT}/JAVA/harvester/harvestingSet.jar
+JAVA=/usr/bin/java
+OUTPUTDIR=${ROOT}/agrisMetaCat/${SET}
+
+for oaiset in p15738coll2
+do
+  echo "harvesting AGLRGSG" ${set}
+  ${JAVA} -jar ${HARVESTER} ${OAITARGET} ${OUTPUTDIR}/ ${OAIFORMAT} ${oaiset}
+done
+echo "Done!"

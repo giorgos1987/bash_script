db2c178f224b8381d3444a36b3ff608518ea79cc
Giorgos Papoutsis <giorgos1987@users.noreply.github.com>
2015-11-25 11:00:01 +0200

    Update clearFolders.sh

--- clearFolders.sh
+++ clearFolders.sh
@@ -19,7 +19,7 @@ if [ "$#" != "1" ]
 
 
     echo "clearing directories"
-#    rm -r ${REPOSITORY}/${dir}/*
+#    rm -r ${REPOSITORY}/${dir}/*.xml
     done
 echo "$(date) The folders ES1  MY0  RU1   RUS  ESB   RU2   ESB AL1 F10 BG0  RU3  SK0 ID0  ORGEPRINTS  RUA TAPBIBLIO BR2  GE0  p15738coll2    RUC  BR3 IN1 PE2 RUD  TR2 BRF  greenet IN4 QB1  RUE  TTT BRK IN5 QT0 RUG UA0 CO0  QTA  RUH  US2 GREENIDEAS RUK   Voa3r_ORGEPRINTS DEC  RISM  RUP  XF0 cleared"
      
